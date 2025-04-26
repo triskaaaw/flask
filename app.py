dd694b8ded33237d3db113e73c0f6fe1553fa1c1
triskaaaw <triskawidiantari@gmail.com>
2025-04-27 00:21:49 +0800

    -

--- app.py
+++ app.py
@@ -585,6 +585,6 @@ def get_like_recipes():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
